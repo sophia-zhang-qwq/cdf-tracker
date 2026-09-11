ed15f9ef0fdf08b709efdb0c6142fe84f0312528
Wenminqi Zhang <wenminqizhang@WenminqideMacBook-Pro-2.local>
2026-09-11 19:47:20 +0800

    favor update

--- favor/favor.py
+++ favor/favor.py
@@ -13,12 +13,12 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 from common.headers import HEADERS, COOKIES, get_headers
 
 #URL = "https://www.cdf-beauty.com/api/prod/sqactivityproductsearch"
-URL = "https://www.cdf-beauty.com/api/nodesearch/sqsearch"
+URL = "https://www.cdf-beauty.com/api/social/sqgetcollectlist"
 # -------------------------
 # 填自己的 Header
 # -------------------------
 #referer = "https://www.cdf-beauty.com/myorderlist?selectedTabState=0&source=usercenter"
-referer = "https://www.cdf-beauty.com/showactivity?pageId=57179&soldOutShowType=1"
+referer = "https://www.cdf-beauty.com/mycollection"
 headers = get_headers(referer)
 
 all_products = []
@@ -27,33 +27,27 @@ all_products = []
 session = requests.Session()
 session.headers.update(headers)
 session.cookies.update(COOKIES)
+favor_token = "2C5FBFA3CAB5CF65ED75D7A68373927085C413F2D1FC4AE9A2C9336FAC35E9F0890BA0A7D317F27B9A3D97F277FF4319DAEA26752A1D56B6"
+favor_ymt = "format=json&appid=71&accesstoken=2C5FBFA3CAB5CF657BD268641840AF206586FAED73C79637DEAF025B52B56367279D2E7C27A2A48462D1FA6A593C96CDDB370432B9514C68&userid=626699686&os=iOS&client=iOS&requestid=a34b7689-8ae0-c586-b622-fb87284019ec&idfa=e38dacf2-22ce-79ce-b7c1-910aca56b78f&imei=e38dacf2-22ce-79ce-b7c1-910aca56b78f&mchId=604163145&language=zh_TW"
+session.cookies.update({"_token": favor_token})
+session.headers["ymt-pars"] = favor_ymt
 
 page = 1
 all_products = []
 
 payload = {
-    "pageIndex": 1,
-    "refreshProduct": True,
-    #"partId": 1133751,
-    "partId": 1144863,
+    "pageIndex": 1
 }
 
 r = session.post(URL, json=payload)
 data = r.json()
-#total_products = data["count"]
-total_products = data["recordCount"]
-print(f"Total products: {total_products}")
-#total_pages是总页数, page_size是每页的商品数量
-total_pages = data["count"]
-print(f"Total pages: {total_pages}")
-page_size = math.ceil(total_products / total_pages)
-print(f"#Products each page: {page_size}")
 
 # -------------------------
 # iterate through pages to fetch member-exclusive products
 # -------------------------
-#while True:
-for page in range(1, total_pages + 1):
+page = 1
+while True:
+#for page in range(1, total_pages + 1):
     payload["pageIndex"] = page
 
     # sqactivityproductsearch API does not allow GET requests, 
@@ -69,27 +63,19 @@ for page in range(1, total_pages + 1):
     # 这里可能叫 data / result / orders / list
     # 根据实际 response 调一下即可
     # -------------------------
-    orders = data.get("list", [])
+    orders = data.get("productList", [])
 
     if len(orders) == 0:
         break
 
     all_products.extend(orders)
-
-    if page % 10 == 0 or page == total_pages:
-        print(f"Page {page}/{total_pages} | " f"Products: {len(all_products)}")
-
     page += 1
     # 搞个随机 让傻逼对面认为我们是人类,不要被封
     # time.sleep(0.5)
     time.sleep(random.uniform(0,1))
 
 print("=" * 60)
-print(f"Summary: Page {total_pages} | Products: {len(all_products)}")
-
-# 保存完整 JSON
-with open("outlet.json","w",encoding="utf-8") as f:
-    json.dump(all_products,f,ensure_ascii=False,indent=2)
+print(f"Summary: Page {page} | Products: {len(all_products)}")
 
 rows = []
 
@@ -98,49 +84,27 @@ for p in all_products:
     price = p.get("price")
     #original_price = p.get("originalPriceCopy")
     original_price = p.get("originalPrice")
-                            
+    # check if yes/no discount
+    if not p.get("activityDiscount"):
+        original_price = price        
+
     rows.append({
-
         "product_id": p.get("id"),
-
-        "activity_product_id": p.get("activityProductId"),
-
         "brand_id": p.get("brandId"),
-
-        "brand": p.get("brandName"),
-        "brand_en": p.get("brandEnName"),
-
         "name": p.get("name"),
 
-        "category": p.get("categoryId"),
-
-        "subcategory": p.get("thirdCategoryId"),
-
         "price": price,
-
         "original_price": original_price,
+        "vipPrice": p.get("vipPrice"),
+    
+        "discount": round(price*10.0/original_price,1) if original_price else None,
+        "activity_stock": p.get("stockStatus"),
 
-        "discount": p.get("activityDiscount"),
-
-        "stock": p.get("stock"),
-
-        "activity_stock": p.get("activityStock"),
-
-        "sell_num": p.get("sellNum"),
-
-        "activity_id": p.get("activityId"),
-
-        "activity_type": p.get("activityType"),
-
-        "catalog_id": p.get("catalogId"),
-
-        #"sku": p.get("minCatalogPriceVo", {}).get("sku"),
-        "sku":p.get("catalogInfo",{}).get("sku")
-
-    })
+        "discountInfo": p.get("discountInfo"),
+        "promotionInfo": p.get("promotionInfo"),})
 
 df = pd.DataFrame(rows)
 
-df.to_csv("outlet.csv",index=False,encoding="utf-8-sig")
+df.to_csv("favor.csv",index=False,encoding="utf-8-sig")
 print("=" * 60)
 print("Done.")
